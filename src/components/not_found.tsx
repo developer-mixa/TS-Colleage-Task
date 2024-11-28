37b7416e0b8f9b64cfbc6e58b3c5a0dcbe42c75f
developer-mixa <mixail_081177@mail.ru>
2024-11-28 15:38:06 +0300

    feat: Added base router

---
+++ src/components/not_found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+      <div>
+        <h1>404 Not Found</h1>
+        <p>The requested resource was not found.</p>
+      </div>
+    );
+  };
+
+export default NotFound
