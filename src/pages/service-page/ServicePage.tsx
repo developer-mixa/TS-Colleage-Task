200e1a54a1daf1cc2b451e651def61f8aac3bbd0
developer-mixa <mixail_081177@mail.ru>
2024-11-29 14:00:39 +0300

    feat: Added base file for Service Page

---
+++ src/pages/service-page/ServicePage.tsx
@@ -0,0 +1,10 @@
+const ServicePage = () => {
+    return (
+      <div>
+        <h1>404 Not Found</h1>
+        <p>The requested resource was not found.</p>
+      </div>
+    );
+  };
+
+export default ServicePage
