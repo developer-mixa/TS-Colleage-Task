9601e5b351a2bba6b45a605d2966efdd97a5af22
developer-mixa <mixail_081177@mail.ru>
2024-12-03 12:12:13 +0300

    feat: Added full name

--- src/components/navigation-bar/NavigationBar.tsx
+++ src/components/navigation-bar/NavigationBar.tsx
@@ -1,12 +1,50 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import styles from './NavigationBar.module.css';
 import { logout, selectIsAuth } from '../../store';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  image: string;
+}
 
 const NavigationBar = () => {
   const isAuthenticated = useSelector(selectIsAuth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+
+  // Fetch user data on mount
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/auth/me", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Ошибка при получении данных пользователя');
+        }
+
+        const data = await response.json() as User;
+        setUser(data);
+      } catch (error) {
+        console.error('Ошибка при получении данных пользователя:', error);
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <div className={styles.toolbar}>
@@ -17,7 +55,11 @@ const NavigationBar = () => {
       <div className={styles.rightSection}>
         {isAuthenticated ? (
           <>
-            <button className={styles.logoutButton} onClick={() => navigate('/profile')}>Профиль</button>
+            {user && (
+              <button className={styles.profileButton} onClick={() => navigate('/profile')}>
+                {user.firstName} {user.lastName}
+              </button>
+            )}
             <button className={styles.logoutButton} onClick={() => dispatch(logout())}>Выйти</button>
           </>
         ) : (
